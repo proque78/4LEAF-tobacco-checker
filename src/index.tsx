c2bb87454fe9f78464450ebf6889032e6152fe38
proque78 <43524994+proque78@users.noreply.github.com>
2026-01-12 22:31:20 -0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,8 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
 const rootElement = document.getElementById("root");
-if (!rootElement) throw new Error('Could not find root element to mount to');
+if (!rootElement) throw new Error("Could not find root element to mount to");
 
 createRoot(rootElement).render(<App />);
-  // Temporarily remove StrictMode if it still errors (see step #3)
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
